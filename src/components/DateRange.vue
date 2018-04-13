27e72ad45b3ae5a3a9428bad1d8d5ff72fc3a454
chuiliu <zhengchuiliu@foxmail.com>
2018-04-13 18:00:01 +0800

    update

---
+++ src/components/DateRange.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <router-link to="/c">
+      <input type="text" v-model="startDate">
+    </router-link>
+    <router-link to="/c">
+      <input type="text" v-model="endDate">
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  props: {
+    start: '',
+    end: ''
+  },
+  data () {
+    return {
+      startDate: this.start,
+      endDate: this.end
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
